8f2d9b06000fd0dd1b0644dfcd3645119854247b
Sheldon Kryger <s.d.kryger@gmail.com>
2019-10-13 04:26:37 +0000

    add example web api source

---
+++ python/apiCarbonIntensity.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import paho.mqtt.client as mqtt
+import time
+
+headers = {
+  'Accept': 'application/json'
+}
+
+client = mqtt.Client()
+client.connect("127.0.0.1")
+client.loop_start()
+
+while True:
+    r = requests.get('https://api.carbonintensity.org.uk/intensity', params={}, headers = headers)
+    response = r.json()
+    print response
+    print response["data"][0]["intensity"]["actual"]
+    val = response["data"][0]["intensity"]["actual"]
+    client.publish('data/numeric/carbonIntensity','%.02f'%val)
+    time.sleep(60)
