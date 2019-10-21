368a3267454c363b6b847742f3d31b1f315efdb0
Sheldon Kryger <s.d.kryger@gmail.com>
2019-10-21 06:53:49 -0600

    additional chart cleanup

--- js/src/index.js
+++ js/src/index.js
@@ -2,7 +2,49 @@ import Vue from 'vue';
 import VueMqtt from 'vue-mqtt';
 var $ = require("jquery");
 require('bootstrap');
-import Visualisation from './components/Visualisation.vue';
+//import Visualisation from './components/Visualisation.vue';
+//import Chart from 'chart.js';
+var mychartLabels = [];
+var mychartDatasets = [];
+window.onload = function(){
+	var ctx2 = document.getElementById('myChart2').getContext('2d');
+	window.myChart3 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+		labels: mychartLabels,
+		datasets: mychartDatasets
+	},
+    options: { 
+		responsive: true,
+		maintainAspectRatio: false,
+		animation: false,
+		scales:{
+			xAxes:[{
+			  gridLines: {
+				  display: true,
+				  drawBorder: true,
+				  drawOnChartArea: false,
+				}
+			}],
+			yAxes:[{
+			  gridLines: {
+				  display: true,
+				  drawBorder: true,
+				  drawOnChartArea: false,
+				}
+			}]
+		},
+		elements:{
+			point:{
+				radius:0
+			}
+		}
+	}
+});
+console.log("sucess");
+};
+
+
 
 var app = new Vue({
     el: '#app',
@@ -21,9 +63,9 @@ var app = new Vue({
 		logFileName: '',
 		logFilePath: '',
 		logFileSize: '',
-		graphLabels:[],
-		graphDatasets:[],
-		graphData:{},
+		//graphLabels:[],
+		//graphDatasets:[],
+		//graphData:{},
 		chartColors: [
 			'rgb(255, 99, 132)',
 			'rgb(255, 159, 64)',
@@ -35,12 +77,9 @@ var app = new Vue({
 		],
 		graphCounter:0
 	},
-	components: {
-        Visualisation
-    },
     mounted(){
-		this.graphData.labels = this.graphLabels;
-		this.graphData.datasets = this.graphDatasets;
+		//this.graphData.labels = this.graphLabels;
+		//this.graphData.datasets = this.graphDatasets;
 	console.log("server ip address is: "+ipAddress);
 	if(ipAddress == '::1')
 	    ipAddress = 'localhost';
@@ -75,7 +114,8 @@ var app = new Vue({
 		    t.lastUpdated = updatedAt;
 			self.numericData.push(t);
 			t = {label:name,backgroundColor:self.chartColors[self.numericData.length % 7],borderColor:self.chartColors[self.numericData.length % 7],data:[],lineTension:0,fill:false};
-			self.graphDatasets.push(t);
+			//self.graphDatasets.push(t);
+			mychartDatasets.push(t);
 		}
 	    } else if (topicArray[0] == 'processor' && topicArray[1] == 'heartbeat'){
 		this.lastProcessorUpdate = new Date();
@@ -134,15 +174,22 @@ var app = new Vue({
 			var maxLength = 3600;
 			//self.graphLabels.push(self.graphLabels.length);
 			for(i = 0; i<self.numericData.length;i++){
-				self.graphDatasets[i].data.push(self.numericData[i].latestValue);
-				if(self.graphDatasets[i].data.length>maxLength)
-					self.graphDatasets[i].data.splice(0,1);
+				//self.graphDatasets[i].data.push(self.numericData[i].latestValue);
+				mychartDatasets[i].data.push(self.numericData[i].latestValue);
+				if(mychartDatasets[i].data.length>maxLength)
+					mychartDatasets[i].data.splice(0,1);
+				//if(self.graphDatasets[i].data.length>maxLength)
+					//self.graphDatasets[i].data.splice(0,1);
 			}
-			Vue.set(self.graphLabels,self.graphLabels.length,self.graphCounter);
+			//Vue.set(self.graphLabels,self.graphLabels.length,self.graphCounter);
+			mychartLabels.push(self.graphCounter);
+			if(mychartLabels.length>maxLength)
+				mychartLabels.splice(0,1);
 			self.graphCounter++;
-			if(self.graphLabels.length>maxLength){
-				self.graphLabels.splice(0,1);
-			}
+			//if(self.graphLabels.length>maxLength){
+			//	self.graphLabels.splice(0,1);
+			//}
+			window.myChart3.update();
 		}
     }
 });
